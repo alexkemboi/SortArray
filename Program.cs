bf2a070c7097e1584a58e2326f328d754cf43e35
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-14 15:43:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class SortArray
+{
+    public static void  Sort(int[] arr){
+        Array.Sort(arr);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args){
+        int[] array = { 5, 7, 1, 8, 3, 9, 2, 6 };
+         SortArray.Sort(array);
+        foreach(int number in array){
+            Console.Write(number + " ");
+        }
+    }
+}
